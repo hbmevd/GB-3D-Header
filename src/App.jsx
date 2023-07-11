40b7c694f8fc3ce80e1411ec91142d42fac4574e
Jacobferdinand <hello@jacobfs.tech>
2023-07-11 10:40:21 +0200

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Flamingo from './assets/Flamingo_02'
 import { Environment, Grid } from '@react-three/drei'
 import CameraRig from './assets/CameraRig'
 import { ACESFilmicToneMapping } from 'three'
-import gsap from 'gsap'
+
 
 import {
   Bloom,
@@ -21,79 +21,7 @@ import {
 } from '@react-three/postprocessing'
 
 export default function App() {
-  // LOADING ANIMATION
-  let onceLoadet = gsap.timeline({ paused: true })
-  onceLoadet
-    .set('.loading-transition-wrapper', { opacity: 0, display: 'none' })
-    .to('.blur-filter', { backdropFilter: 'blur(0px)', duration: 2 })
 
-  function playLoadingAnimation() {
-    gsap.set('.loading-transition-wrapper', { display: 'block' })
-    const load = gsap.timeline({
-      paused: 'true',
-    })
-    load
-      .to('#percent , #bar', {
-        duration: 0.1,
-        opacity: 0,
-        zIndex: -1,
-      })
-      .to('.progress', {
-        duration: 0.8,
-        width: '0%',
-      })
-      .to(
-        '.loader_lottie',
-        {
-          duration: 0.6,
-          opacity: 0,
-        },
-        '-=.5'
-      )
-      .to(
-        '.loader_content',
-        {
-          duration: 0.6,
-
-          ease: 'power1.inOut',
-          display: 'none',
-          marginBottom: '100%',
-        },
-        '-=.5'
-      )
-      .to('.loading-transition-wrapper', {
-        duration: 0,
-        display: 'none',
-      })
-      .to('.blur-filter', { backdropFilter: 'blur(0px)', duration: 2 })
-
-    var id,
-      width = 1
-    function loading() {
-      id = setInterval(frame, 25)
-    }
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id)
-        load.play()
-      } else {
-        width++
-        document.getElementById('loadingbar').style.width = width + '%'
-        document.getElementById('percent').innerHTML = width + '%'
-      }
-    }
-
-    window.onload = function () {
-      loading()
-    }
-  }
-
-  if (!sessionStorage.getItem('hasLoadedOnce')) {
-    sessionStorage.setItem('hasLoadedOnce', true)
-    playLoadingAnimation()
-  } else {
-    onceLoadet.play()
-  }
 
   return (
     <Canvas
